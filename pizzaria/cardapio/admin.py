70d00cc1ad0e5e042e37d5ab9ad04ff28efd4b08
Luís Eduardo <68335191+NightOwl993@users.noreply.github.com>
2025-09-09 19:43:50 +0000

    aaaa

--- pizzaria/cardapio/admin.py
+++ pizzaria/cardapio/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
+from .models import TipoIngrediente, TipoPizza, Ingrediente, Pizza, IngredientePizza
 # Register your models here.
+
+@admin.register(TipoPizza)
+class TipoPizzaAdmin(admin.ModelAdmin):
+    list_display = ['nome','descricao']
+
+@admin.register(TipoIngrediente)
+class TipoIngredienteAdmin(admin.ModelAdmin):
+    list_display=['nome', 'tipo_ingrediente', 'preco_formatado', 'disponivel']
+
+#Filtros
+list_filter = ['tipo_ingrediente', 'disponivel']
+
+def preco_formatado(self,obj):
+    return f'R$ {obj.preco_por_unidade:.3f}/{obj.unidade_medida}'
+
+#Rotulo
+preco_formatado.short_description = 'Preço'
