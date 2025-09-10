f49f1b903351fdde508996a8007fbd04f771d493
Luís Eduardo <68335191+NightOwl993@users.noreply.github.com>
2025-09-10 17:31:14 +0000

    aaaa

--- pizzaria/cardapio/admin.py
+++ pizzaria/cardapio/admin.py
@@ -20,6 +20,21 @@ def preco_formatado(self,obj):
 #Rotulo
 preco_formatado.short_description = 'Preço'
 
+@admin.register(Ingrediente)
+class IngredienteAdmin(admin.ModelAdmin):
+    # Campos que aparecem na listagem de Ingredientes
+    list_display = ['nome', 'tipo_ingrediente', 'preco_formatado', 'disponivel']
+
+    # Filtros laterais para facilitar busca
+    list_filter = ['tipo_ingrediente', 'disponivel']
+    
+    # Função personalizada para formatar o preço (ex: R$ 5.00/kg)
+    def preco_formatado(self, obj):
+        return f'R$ {obj.preco_por_unidade:.2f}/{obj.unidade_medida}'
+
+    # Nome da coluna no Admin
+    preco_formatado.short_description = 'Preço'
+
 class IngredientePizzaInline(admin.TabularInline):
     #Definir Model
     model = IngredientePizza
@@ -39,3 +54,6 @@ class PizzaAdmin(admin.ModelAdmin):
         return f'R$ {obj.preco_total:.2f}'
 
         preco_total_formatado.short_description = 'Preço Total'
+
+admin.site_header = "SENAC PIZZA - Administração"
+admin.site_title = "Pizzaria Admin"
--- pizzaria/cardapio/views.py
+++ pizzaria/cardapio/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import TipoIngrediente, TipoPizza, Ingrediente, Pizza, IngredientePizza
 
 # Create your views here.
+def home(request):
+    """Pagina Incial"""
+    pizzas_salgadas = Pizza.objects.filter(tipo_pizza_nome="Salgada", ativa=True)
+    pizzas_doces = Pizza.objects.filter(tipo_pizza_nome="Doce", ativa=True)
+
+    #Dicionario com os dadso que eu quero mostrar na tela
+    context = {
+        "pizzas_salgadas" : pizzas_salgadas,
+        "pizzas_doces" : pizzas_doces
+    }
+    
+    #Renderizar - mostrar na tela
+    return render(request, 'cardapio/home.html', context)
